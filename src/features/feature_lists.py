4b7d473c2b0cb42cd050228f06c69cb5e4f19dfb
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2022-08-23 21:26:18 -0400

    Temporarily stop using other features

--- src/features/feature_lists.py
+++ src/features/feature_lists.py
@@ -690,10 +690,10 @@ def get_desktop_features():
     # print(load_feature_file("kit_f1.pickle"))
     # input("pickle")
     desktop_kht_features = top_feature_KIT(load_feature_file("kht.pickle"))
-    desktop_kit_features_f1 = top_feature_KIT(load_feature_file("kit_f1.pickle"))
-    desktop_kit_features_f2 = top_feature_KIT(load_feature_file("kit_f2.pickle"))
-    desktop_kit_features_f3 = top_feature_KIT(load_feature_file("kit_f3.pickle"))
-    desktop_kit_features_f4 = top_feature_KIT(load_feature_file("kit_f4.pickle"))
+    # desktop_kit_features_f1 = top_feature_KIT(load_feature_file("kit_f1.pickle"))
+    # desktop_kit_features_f2 = top_feature_KIT(load_feature_file("kit_f2.pickle"))
+    # desktop_kit_features_f3 = top_feature_KIT(load_feature_file("kit_f3.pickle"))
+    # desktop_kit_features_f4 = top_feature_KIT(load_feature_file("kit_f4.pickle"))
 
     print(type(desktop_kht_features))
     # input()
@@ -701,10 +701,10 @@ def get_desktop_features():
     return np.concatenate(
         (
             np.array(desktop_kht_features),
-            np.array(desktop_kit_features_f1),
-            np.array(desktop_kit_features_f2),
-            np.array(desktop_kit_features_f3),
-            np.array(desktop_kit_features_f4),
+            # np.array(desktop_kit_features_f1),
+            # np.array(desktop_kit_features_f2),
+            # np.array(desktop_kit_features_f3),
+            # np.array(desktop_kit_features_f4),
         ),
         axis=1,
     )
