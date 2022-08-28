936ffbdafd2c87e9a59eab79c5a07fcd360e8512
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2022-08-28 10:18:38 -0400

    Get the n most frequent digraphs from the dataset

--- src/data_processor/aggregator.py
+++ src/data_processor/aggregator.py
@@ -1,5 +1,11 @@
 import os
 import pandas as pd
+import collections
+from tqdm import tqdm
+
+
+def flatten(l):
+    return [item for sublist in l for item in sublist]
 
 
 class KeystrokeFile:
@@ -32,7 +38,7 @@ class KeystrokeFile:
     def digraphs(self):
         i = 0
         data = self.all_keys()
-        print(data)
+        # print(data)
         # input()
         digraphs = []
         while i < len(data):
@@ -45,8 +51,44 @@ class KeystrokeFile:
             i += 1
         return digraphs
 
+    def collapse_into_strings(self, digraphs):
+        ret = []
+        for digraph in digraphs:
+            string = digraph[0] + digraph[1]
+            ret.append(string)
+        return ret
+
+    def digraph_frequency(self, digraphs):
+        digraph_strs = self.collapse_into_strings(digraphs)
+        frequency = collections.Counter(digraph_strs)
+        return dict(frequency)
+
+    def n_most_common_digraphs(self, digraphs_frequencies_dict, n: int):
+        ret = []
+        sorted_frequencies = sorted(
+            digraphs_frequencies_dict.items(), key=lambda x: x[1]
+        )
+        if n <= 0:
+            raise ValueError("N must be greater than 0")
+        d = list(sorted_frequencies)[-n:]
+        for digraph_set in d:
+            ret.append(digraph_set[0])
+        return ret
+
+
+def all_common_digraphs(n: int):
+    p = os.path.join(os.getcwd(), "data", "km")
+    onlyfiles = [f for f in os.listdir(p) if os.path.isfile(os.path.join(p, f))]
+    all = []
+    for file in tqdm(onlyfiles):
+        if file.endswith(".csv"):
+            kf = KeystrokeFile(os.path.join(os.getcwd(), "data", "km", file))
+            digraphs = kf.digraphs()
+            all.append(digraphs)
+    flat = flatten(all)
+    frequencies = kf.digraph_frequency(flat)
+    return kf.n_most_common_digraphs(frequencies, n)
+
 
 if __name__ == "__main__":
-    kf = KeystrokeFile(os.path.join(os.getcwd(), "data", "subset", "f_4_fpd1.csv"))
-    digraphs = kf.digraphs()
-    print(digraphs)
+    print(all_common_digraphs(30))
