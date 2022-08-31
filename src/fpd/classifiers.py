28e6d42034b23c2ee8028b348e74ef8696e4a295
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2022-08-30 22:08:38 -0400

    Code cleanup and trying out an xgb classifier
    Interestingly, xgb gives about 94% accuracy but I don't know how meaningful it is

--- src/fpd/classifiers.py
+++ src/fpd/classifiers.py
@@ -2,10 +2,11 @@ from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier
-import numpy as np
 from fpd.dataset import Dataset
+import xgboost as xgb
+import numpy as np
 
-
+# NOTE: Takes WAY too long to run (7+ hours) and still not running to completion
 def create_svm():
     fp = Dataset(
         "/Users/alvinkuruvilla/Dev/keystroke-research/keystroke-alias/all_features.csv"
@@ -54,8 +55,45 @@ def random_forrest():
     print(y_test)
     input()
     forest = RandomForestClassifier(
-        criterion="gini", n_estimators=5, random_state=1, n_jobs=2
+        criterion="gini", n_estimators=5, random_state=1, n_jobs=100
     )
     forest.fit(X_train, y_train)
     y_pred = forest.predict(X_test)
     print("Accuracy: %.3f" % metrics.accuracy_score(y_test, y_pred))
+
+
+def xgb_classifier():
+    fp = Dataset(
+        "/Users/alvinkuruvilla/Dev/keystroke-research/keystroke-alias/all_features.csv"
+    )
+    y = fp.target()
+    X_train, X_test, y_train, y_test = train_test_split(
+        fp.as_numpy_array(),
+        fp.target(),
+        test_size=0.3,
+        random_state=1,
+        stratify=y,
+    )  # 70% training and 30% test
+    print(X_train)
+    input()
+    print(X_test)
+    input()
+    print(y_train)
+    input()
+    print(y_test)
+    input()
+    dtrain = xgb.DMatrix(X_train, label=y_train)
+    dtest = xgb.DMatrix(X_test, label=y_test)
+    param = {
+        "max_depth": 3,  # the maximum depth of each tree
+        "eta": 0.3,  # the training step for each iteration
+        "silent": 1,  # logging mode - quiet
+        "objective": "multi:softprob",  # error evaluation for multiclass training
+        "num_class": 3,  # the number of classes that exist in this dataset
+    }
+    num_round = 20  # the number of training iterations
+    bst = xgb.train(param, dtrain, num_round)
+    preds = bst.predict(dtest)
+    best_preds = np.asarray([np.argmax(line) for line in preds])
+    print("Precision:", metrics.precision_score(y_test, best_preds, average="macro"))
+    print("Accuracy: %.3f" % metrics.accuracy_score(y_test, best_preds))
--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from core.tasks.xgb_regression_age import run_age_xgb_regression
 # from custom.models.rnn_gender import train_model
 from core.tasks.rnn_gender_task import train_model
 from fpd.feature_gen import generate_features_file
-from fpd.classifiers import create_svm, random_forrest
+from fpd.classifiers import create_svm, random_forrest, xgb_classifier
 
 install()
 from custom.features.fe import (
@@ -17,34 +17,38 @@ from custom.features.fe import (
     get_all_users_features_KIT,
 )
 
+
+def pickle_features():
+    dir_name = "data/"
+    dir_path = os.path.join(os.getcwd(), dir_name, "km/")
+    selected_profile_path = os.path.join(dir_path)
+    desktop_kht_features = get_all_users_features_KHT(selected_profile_path)
+    with open("desktop_kht_feature_dictionary.pickle", "wb") as handle:
+        pickle.dump(desktop_kht_features, handle)
+    (
+        desktop_kit_features_f1,
+        desktop_kit_features_f2,
+        desktop_kit_features_f3,
+        desktop_kit_features_f4,
+    ) = get_all_users_features_KIT(selected_profile_path)
+    with open("desktop_kit_feature_f1_dictionary.pickle", "wb") as handle:
+        pickle.dump(desktop_kit_features_f1, handle)
+    with open("desktop_kit_feature_f2_dictionary.pickle", "wb") as handle:
+        pickle.dump(desktop_kit_features_f2, handle)
+    with open("desktop_kit_feature_f3_dictionary.pickle", "wb") as handle:
+        pickle.dump(desktop_kit_features_f3, handle)
+    with open("desktop_kit_feature_f4_dictionary.pickle", "wb") as handle:
+        pickle.dump(desktop_kit_features_f4, handle)
+
+
 if __name__ == "__main__":
     # TODO: Pickle dump the desktop pickle files and add function for advanced word pickle file
     dir_name = "data/"
     dir_path = os.path.join(os.getcwd(), dir_name, "km/")
     selected_profile_path = os.path.join(dir_path)
-    # desktop_kht_features = get_all_users_features_KHT(selected_profile_path)
-    # with open("desktop_kht_feature_dictionary.pickle", "wb") as handle:
-    #     pickle.dump(desktop_kht_features, handle)
-    # (
-    #     desktop_kit_features_f1,
-    #     desktop_kit_features_f2,
-    #     desktop_kit_features_f3,
-    #     desktop_kit_features_f4,
-    # ) = get_all_users_features_KIT(selected_profile_path)
-    # with open("desktop_kit_feature_f1_dictionary.pickle", "wb") as handle:
-    #     pickle.dump(desktop_kit_features_f1, handle)
-    # with open("desktop_kit_feature_f2_dictionary.pickle", "wb") as handle:
-    #     pickle.dump(desktop_kit_features_f2, handle)
-    # with open("desktop_kit_feature_f3_dictionary.pickle", "wb") as handle:
-    #     pickle.dump(desktop_kit_features_f3, handle)
-    # with open("desktop_kit_feature_f4_dictionary.pickle", "wb") as handle:
-    #     pickle.dump(desktop_kit_features_f4, handle)
 
     # run_age_xgb_regression()
     # train_model("Gender")
     # train_model_cnn("Gender")
-    with open("kit_f2.pickle", "rb") as handle:
-        data = pickle.load(handle)
-    # print(data)
     # generate_features_file(selected_profile_path)
-    random_forrest()
+    xgb_classifier()
